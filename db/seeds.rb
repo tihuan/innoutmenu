bc7bad736255ab4930f3995aaca131faa580b844
Timmy Huang <timmyhuang0404@gmail.com>
2014-07-25 23:42:05 -0700

    temp seed file. not updated yet

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+ActiveRecord::Base.transaction do
+  5.times do
+      Account.create(
+        fname: Faker::Name.first_name,
+        lname: Faker::Name.last_name,
+        address: Faker::Address.street_address,
+        phone: Faker::PhoneNumber.phone_number,
+        birthdate: Faker::Business.credit_card_expiry_date,
+        email: Faker::Internet.email,
+        password: Faker::Internet.password,
+        memo: Faker::Company.bs
+      )
+
+      Service.create(
+        name: Faker::Commerce.product_name,
+        price_in_cents: Faker::Address.building_number
+      )
+
+      Appointment.create(
+        date: Faker::Business.credit_card_expiry_date,
+        hour: '1200',
+        account_id: rand(2),
+        service_id: rand(2),
+      )
+  end
+end
